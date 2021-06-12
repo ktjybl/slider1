69b792de2a17500a27c953a3bf680b9272c825e3
ktjybl <64632527+ktjybl@users.noreply.github.com>
2021-06-12 13:40:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-const dotts=()=>{
-const controllToggles=document.querySelectorAll('.controll-toggle');
-const slideItemElems=document.querySelectorAll('.slide-item');
-const hide=()=>{
-for( let i=0; i<controllToggles.length; i++){
-console.log(controllToggles);
-controllToggles[i].classlList.remove('active');
-   };
-}
-for(let i=0; i<controllToggles.length; i++){
-      controllToggles[i].addEventListener('click', ()=>{
-      hide();
-      controllToggles[i].classList.add('active'); 
-      slideItemElems[i].classList.add('active');
-       });
-   };
+const sliderWrapper=document.querySelector('.slider');
+const sliderInner=document.querySelector('.slider-items');
+const nextElem=document.querySelector('.btn-nxt');
+const prevElem=document.querySelector('.btn-prev');
+const slides=document.querySelectorAll('.slide-item');
+
+let width;
+let count=0;
+let slideToShow=1;
+
+
+
+const slide=()=>{
+for(item of slides){
+item.style.height='auto';
+item.style.width=width+'px';
 };
-dotts();
+};
+
+const scroll=()=>{
+   sliderInner.style.transform='translate(-'+count*width / slideToShow+'px)';
+};
+
+
+
+const start=()=>{
+  width=sliderWrapper.offsetWidth;
+  sliderInner.style.width=width*slides.length / slideToShow +'px';
+  slide();
+  
+  scroll();
+};
+const setEvent=()=>{
+window.addEventListener('resize', start);
+};
+const destroyEvent=()=>{
+window.removeEventListener('resize', start);
+};
+setEvent();
+destroyEvent();
+start();
+
+nextElem.addEventListener('click', ()=>{ 
+count++;
+if(count>=slides.length){
+  count=0;
+}
+scroll()});
+
+prevElem.addEventListener('click', ()=>{ 
+count--;
+if(count<0){
+  count=slides.length-1;
+}
+scroll()
+});
